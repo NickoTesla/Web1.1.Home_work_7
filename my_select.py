09e105766328b51c3a730c07ded2c2ff7596d43f
NickoTesla <mykolandwirt@gmail.com>
2023-07-10 13:10:20 -0600

    update

--- my_select.py
+++ my_select.py
@@ -20,4 +20,64 @@ def select_2(session, subject_name):
                   .first()
 
 
-def select_
+def select_3(session, subject_name):
+    return session.query(Group.name, func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
+                  .join(Student) \
+                  .join(Grade) \
+                  .join(Subject) \
+                  .filter(Subject.name == subject_name) \
+                  .group_by(Group.id) \
+                  .all()
+
+
+def select_4(session):
+    return session.query(func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
+                  .scalar()
+
+
+def select_5(session, teacher_name):
+    return session.query(Subject.name) \
+                  .join(Teacher) \
+                  .filter(Teacher.fullname == teacher_name) \
+                  .all()
+
+
+def select_6(session, group_name):
+    return session.query(Student.fullname) \
+                  .join(Group) \
+                  .filter(Group.name == group_name) \
+                  .all()
+
+
+def select_7(session, group_name, subject_name):
+    return session.query(Student.fullname, Grade.grade) \
+                  .join(Group) \
+                  .join(Grade) \
+                  .join(Subject) \
+                  .filter(Group.name == group_name, Subject.name == subject_name) \
+                  .all()
+
+
+def select_8(session, teacher_name):
+    return session.query(func.round(func.avg(Grade.grade), 2).label('avg_grade')) \
+                  .join(Subject) \
+                  .join(Teacher) \
+                  .filter(Teacher.fullname == teacher_name) \
+                  .scalar()
+
+
+def select_9(session, student_name):
+    return session.query(Subject.name) \
+                  .join(Grade) \
+                  .join(Student) \
+                  .filter(Student.fullname == student_name) \
+                  .all()
+
+
+def select_10(session, student_name, teacher_name):
+    return session.query(Subject.name) \
+                  .join(Teacher) \
+                  .join(Grade) \
+                  .join(Student) \
+                  .filter(Student.fullname == student_name, Teacher.fullname == teacher_name) \
+                  .all()
